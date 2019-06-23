b6f891f4d7aa794c68b4808d36c95ef9818b615f
Katsiaryna Fedarovich <kathefy215@gmail.com>
2019-06-23 22:44:40 +0200

    Repos

--- github.js
+++ github.js
@@ -1,11 +1,24 @@
 class GitHub {
-  constructor() {}
+  constructor() {
+    this.reposCount = 5;
+    this.reposSort = 'created: asc';
+  }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}`);
+
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.reposCount
+      }&sort=${this.reposSort}`
+    );
+
     const profile = await profileResponse.json();
+
+    const repos = await reposResponse.json();
     return {
-      profile
+      profile,
+      repos
     };
   }
 }
