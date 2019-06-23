821e95b950829bd6de0ac25238524181f91bf2ba
Katsiaryna Fedarovich <kathefy215@gmail.com>
2019-06-23 22:24:55 +0200

    Show errors

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ searchUser.addEventListener('keyup', e => {
   if (userText !== '') {
     github.getUser(userText).then(data => {
       if (data.profile.message === 'Not Found') {
-        // show alert
+        ui.showAlert('User not found', 'alert alert-danger');
       } else {
         ui.showProfile(data.profile);
       }
     });
   } else {
+    ui.clearProfile();
   }
 });
